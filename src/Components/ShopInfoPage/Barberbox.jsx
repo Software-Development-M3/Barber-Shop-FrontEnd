83c260e1713209e690ee0eadb3d0610b9402553f
INANATIDAEBOI <65010983@kmitl.ac.th>
2024-10-30 20:12:33 +0700

    add img

--- src/Components/ShopInfoPage/Barberbox.jsx
+++ src/Components/ShopInfoPage/Barberbox.jsx
@@ -4,7 +4,7 @@ const BarberBox = ({name,pic,exp,spec}) =>{
     return(
         <div className="barberBox">
             <img 
-            src = {pic || "src/assets/barberImage.png"} 
+            src = {pic || "https://cdn-icons-png.flaticon.com/512/147/147144.png"} 
             className="barberPic"
             />
             <div className="barberTextContainer">
