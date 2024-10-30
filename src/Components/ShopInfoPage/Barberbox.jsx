7225de153a4a8a1a46c88fe3bb3d8a6607838368
INANATIDAEBOI <65010983@kmitl.ac.th>
2024-10-30 20:11:17 +0700

    add img

--- src/Components/ShopInfoPage/Barberbox.jsx
+++ src/Components/ShopInfoPage/Barberbox.jsx
@@ -4,7 +4,7 @@ const BarberBox = ({name,pic,exp,spec}) =>{
     return(
         <div className="barberBox">
             <img 
-            src = {pic || "https://via.placeholder.com/100"} 
+            src = {pic || "src/assets/barberImage.png"} 
             className="barberPic"
             />
             <div className="barberTextContainer">
